25df318a7c55ed4b58d4fdddd669381072641018
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-09 09:26:35 -0500

    feat: :memo: Metodo match()
    describcion del metodo march()  y ejemplo

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-    ".localeCompare(str, locale, options): Compara dos cadenas en la localización actual"
+    ".match(regex): Devuelve todas las ocurrencias de una expresión regular dentro de una cadena."
 
 
 // EJEMPLO 1
 //Devuelve la primera conincidencia de la palabra encontrada + su posición
-let ejemplo = "Hola";
-let ejemplo_2 = "soy un ejemplo"
-let otro = ejemplo.localeCompare(ejemplo_2);
+let ejemplo = "Hola Soy Un Ejemplo";
+let ejemplo_2 = /[A-Z]/g;
+let otro = ejemplo.match(ejemplo_2);
 console.log(otro);
 
