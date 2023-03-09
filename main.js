7fc05d1c2796ce72d5a4cdf1917177361d6017fb
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-09 09:32:58 -0500

    feat: :memo: toUpperCase() toLowerCase()
    metodos que nos permiten cambiar de mayusculas a minusculas y viscebersa

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 
+".toLowerCase():Devuelve el valor de la cadena convertida a minúsculas. No afecta el valor de la cadena misma.",
+".toUppercase():Devuelve el valor de la cadena convertida a mayúsculas. No afecta el valor de la cadena misma." 
 
 
-".replace():Remplaza partes de un string por carácteres."
 
-
+// EJEMPLO 1
+// EJEMPLO 1
+let ejemplo = "JavaScript"
+let ejemplo1 = "HI CAMPERS"
+let print = ejemplo.replace("ava","-")
+let print1 = ejemplo.toLowerCase()
+console.log(print)
+console.log(print)
 
 
 // EJEMPLO_2 
+// EJEMPLO_2 
 let ejemplo2 = "Hello World"
+let ejemplo3 = "what's up nigga"
 let print2 = ejemplo2.replace("ello", "-")
+let print3 = ejemplo2.toUpperCase()
+console.log(print2)
 console.log(print2)
