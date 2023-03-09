0981ed7fa80a5364778c53cb01a76297b4bf3b54
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-09 09:25:13 -0500

    feat: :memo: Metodo localeCompare()
    Nuevo metodo para comparar

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 
-
-    ".search() : Busca y retorna index"
-
-
-// EJEMPLO  
-
-let ejemplo = "Hola soy un ejemplo";
+    ".localeCompare(str, locale, options): Compara dos cadenas en la localización actual"
 
 
-let otro = ejemplo.search("un");
+// EJEMPLO 1
+//Devuelve la primera conincidencia de la palabra encontrada + su posición
+let ejemplo = "Hola";
+let ejemplo_2 = "soy un ejemplo"
+let otro = ejemplo.localeCompare(ejemplo_2);
 console.log(otro);
-
 
