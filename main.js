9c44bff544afd880c8dfcda727f376dc5ea618c0
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-09 09:36:43 -0500

    feat: :memo: Metodo trimEnd() trimStart()
    los metodos trim end y start con susu ejemplos

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-".trim(): Remueve los espacios en blanco de ambos extremos del string"
-     
+".trimEnd():Remueve los espacios en blanco solo del final del string",
+".trimStart():Remueve los espacios en blanco solo del inicio del string"
 
 
-
-
-
-let ejemplo = "          HI CAMPERS       "
-
-let print = ejemplo.trim()
-
+// EJEMPLO 1
+// Con trimEnd
+let ejemplo = "HI CAMPERS       "
+let print = ejemplo.trimEnd()
+console.log("\nCon .trinmEnd() no se logra apreciar el tanto el cambio, para verlo realmente\ntoca ver el código js :D\n ")
 console.log(ejemplo)
+console.log(print)
+
+// EJEMPLO_2 
+// Con trimStart
+let ejemplo2 = "           what's up nigga          "
+let print2 = ejemplo2.trimStart()
+console.log(ejemplo2)
+console.log(print2)
