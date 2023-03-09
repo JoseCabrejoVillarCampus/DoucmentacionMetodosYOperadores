d398bf4f46659546d3bee1096579a0bb5b362312
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-09 13:11:41 -0500

    feat: :memo: Operadores Logicos
    agregamos 3 operadores logicos, su descripcion y una pequeña muestra de su funcionalidad

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
-//                              Operadores aritméticos
+//                              Operadores lógicos
 
-//Un operador aritmético toma valores numéricos (ya sean literales o variables) como sus operandos 
-//y devuelve un solo valor numérico. Los operadores aritméticos estándar son suma (+), resta (-), 
-//multiplicación (*) y división (/). Estos operadores funcionan como lo hacen en la mayoría de los 
-//otros lenguajes de programación cuando se usan con números de punto flotante (en particular, ten 
-//en cuenta que la división entre cero produce Infinity).
 
-//Además de las operaciones aritméticas estándar (+, -, *, /), JavaScript proporciona los operadores 
-//aritméticos enumerados en la siguiente tabla:
-
-//Residuo (%) 	= Operador binario. Devuelve el resto entero de dividir los dos operandos. 	
-
-12 % 5 //devuelve 2.
-
-//Incremento (++) =Operador unario. Agrega uno a su operando. 
-//Si se usa como operador prefijo (++x), devuelve el valor de su operando después de agregar uno; si se usa como operador sufijo (x++), devuelve el valor de su operando antes de agregar uno. 	Si x es 3, ++x establece x en 4 y devuelve 4, mientras que x++ devuelve 3 y , solo entonces, establece x en 4.
-
-//Decremento (--) 	=Operador unario. Resta uno de su operando. El valor de retorno es análogo al del operador de incremento. 	
-//Si x es 3, entonces --x establece x en 2 y devuelve 2, mientras que x-- devuelve 3 y, solo entonces, establece x en 2.
-
-//Negación unaria (-) 	=Operador unario. Devuelve la negación de su operando. 	
-//Si x es 3, entonces -x devuelve -3.
-
-//Positivo unario (+) 	=Operador unario. Intenta convertir el operando en un número, si aún no lo es. 	
-//+"3" devuelve 3. +true devuelve 1.
-
-//Operador de exponenciación (**) 	Calcula la base a la potencia de exponente, es decir, baseexponente 	
-//2 ** 3 returns 8. 10 ** -1 returns 0.1.
+//Los operadores lógicos se utilizan normalmente con valores booleanos (lógicos); cuando lo son, 
+//devuelven un valor booleano. Sin embargo, los operadores && y || en realidad devuelven el valor 
+//de uno de los operandos especificados, por lo que si estos operadores se utilizan con valores no 
+//booleanos, pueden devolver un valor no booleano. Los operadores lógicos se describen en la siguiente tabla.
+//
+//                                 =>AND Lógico (&&) 	
+//
+//expr1 && expr2 	Devuelve expr1 si se puede convertir a false; de lo contrario, devuelve expr2. Por lo tanto, 
+//cuando se usa con valores booleanos, && devuelve true si ambos operandos son true; de lo contrario, devuelve false.
+//
+//                                 =>OR lógico (||) 	
+//
+//expr1 || expr2 	Devuelve expr1 si se puede convertir a true; de lo contrario, devuelve expr2. Por lo tanto, 
+//cuando se usa con valores booleanos, || devuelve true si alguno de los operandos es true; si ambos son falsos, devuelve false.
+//
+//                                =>NOT lógico (!) 	
+//
+//!expr 	Devuelve false si su único operando se puede convertir a true; de lo contrario, devuelve true.
