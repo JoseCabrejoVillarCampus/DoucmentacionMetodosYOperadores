245b60f6b7e483171b6e909c1c5bcc10fa27140f
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-09 09:15:04 -0500

    feat: :memo: concat()
    documentamos el nuevo metodo concat, acompañado de un ejemplo

--- main.js
+++ main.js
@@ -1,27 +1,16 @@
 let metodo = {
-    "MÉTODO" : "VALOR",
-    "startsWith, endsWith, includes": "Devuelve si o no la cadena comienza, termina o contiene una subcadena especificada."
+    "MÉTODO": "VALOR",
+    "concat": "Combina el texto de dos cadenas y devuelve una nueva cadena."
 };
-
-console.table(metodo);
 console.table(metodo);
 
 
-// EJEMPLO 1
-// EJEMPLO 1
-//Devuelve la primera conincidencia de la palabra encontrada + su posición
-//Devuelve la primera conincidencia de la palabra encontrada + su posición
-let ejemplo = "Hola soy un ejemplo";
-let ejemplo1 = "Hola soy un ejemplo";
-let otro = ejemplo.indexOf("soy")
-let otro1 = ejemplo.startsWith("soy");
-console.log(otro) 
-console.log(otro);
 
 
-// EJEMPLO 1
-let ejemplo2 = "Hola soy el siguiente ejemplo";
-let ejemplo3 = "Hola soy el siguiente ejemplo"
-let otro2 = ejemplo2.endsWith("ejemplo");
-let otro3 = ejemplo2.lastIndexOf("el")
-console.log(otro2);
+// EJEMPLO 
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+
+console.log(array3);
+// Expected output: Array ["a", "b", "c", "d", "e", "f"]
