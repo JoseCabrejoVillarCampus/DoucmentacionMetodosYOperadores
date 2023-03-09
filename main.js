e8e43968071f0ac834def2f3e8c93e13b7ef05be
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-09 09:02:46 -0500

    feat: :rocket: Documentacion metodos string
    Describe los metodos charCodeAt, chartAt, codePointAt con sus respectivos ejemplos
    
    By: Jose Cabrejo

---
+++ main.js
@@ -0,0 +1,15 @@
+let metodo = {
+    "MÉTODO" : "VALOR",
+    "charAt, charCodeAt, codePointAt" : "Devuelve el caracter o código de caracter en la posición especificada en la cadena.",
+};
+console.table(metodo);
+
+// EJEMPLO 1
+let ejemplo = "Hola soy un ejemplo";
+let otro = ejemplo.charAt(0)
+console.log(otro)
+
+// EJEMPLO 2
+let ejemplo2 = "Hola soy el siguiente ejemplo"
+let otro2 = ejemplo2.charCodeAt(3)
+console.log(otro2)
