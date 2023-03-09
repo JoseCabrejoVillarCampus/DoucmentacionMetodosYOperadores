a66f0528f842a7b0e2c87a4c312639dabfeab9de
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-09 09:10:35 -0500

    feat: :memo: subimos los metodos startsWith, endsWith, includes"
    Describcion de los metodos startsWith, endsWith, includes, con sus debidos ejemplos

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 let metodo = {
     "MÉTODO" : "VALOR",
-    "MÉTODO" : "VALOR",
-    "charAt, charCodeAt, codePointAt" : "Devuelve el caracter o código de caracter en la posición especificada en la cadena.",
-    "indexOf, lastIndexOf" : "Devuelve la posición de la subcadena especificada en la cadena o la última posición de la subcadena especificada, respectivamente."
+    "startsWith, endsWith, includes": "Devuelve si o no la cadena comienza, termina o contiene una subcadena especificada."
 };
 
 console.table(metodo);
@@ -12,19 +10,18 @@ console.table(metodo);
 // EJEMPLO 1
 // EJEMPLO 1
 //Devuelve la primera conincidencia de la palabra encontrada + su posición
+//Devuelve la primera conincidencia de la palabra encontrada + su posición
 let ejemplo = "Hola soy un ejemplo";
 let ejemplo1 = "Hola soy un ejemplo";
-let otro = ejemplo.charAt(0)
-let otro1 = ejemplo.indexOf("soy")
-console.log(otro)
+let otro = ejemplo.indexOf("soy")
+let otro1 = ejemplo.startsWith("soy");
 console.log(otro) 
+console.log(otro);
 
 
-// EJEMPLO 2
 // EJEMPLO 1
-let ejemplo2 = "Hola soy el siguiente ejemplo"
+let ejemplo2 = "Hola soy el siguiente ejemplo";
 let ejemplo3 = "Hola soy el siguiente ejemplo"
-let otro2 = ejemplo2.charCodeAt(3)
+let otro2 = ejemplo2.endsWith("ejemplo");
 let otro3 = ejemplo2.lastIndexOf("el")
-console.log(otro2)
-console.log(otro2)
+console.log(otro2);
