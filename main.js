a111119fd6deb2d3c088f5aff1779dbc6f3a5f29
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-09 09:34:54 -0500

    feat: :memo: Metodo trim()
    descripcion y funcionalidad del metodo trim

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-
-".toLowerCase():Devuelve el valor de la cadena convertida a minúsculas. No afecta el valor de la cadena misma.",
-".toUppercase():Devuelve el valor de la cadena convertida a mayúsculas. No afecta el valor de la cadena misma." 
-
+".trim(): Remueve los espacios en blanco de ambos extremos del string"
+     
 
 
-// EJEMPLO 1
-// EJEMPLO 1
-let ejemplo = "JavaScript"
-let ejemplo1 = "HI CAMPERS"
-let print = ejemplo.replace("ava","-")
-let print1 = ejemplo.toLowerCase()
-console.log(print)
-console.log(print)
 
 
-// EJEMPLO_2 
-// EJEMPLO_2 
-let ejemplo2 = "Hello World"
-let ejemplo3 = "what's up nigga"
-let print2 = ejemplo2.replace("ello", "-")
-let print3 = ejemplo2.toUpperCase()
-console.log(print2)
-console.log(print2)
+
+let ejemplo = "          HI CAMPERS       "
+
+let print = ejemplo.trim()
+
+console.log(ejemplo)
