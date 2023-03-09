1ca3b10b86afa8625d42bcf1bf29850fe016d417
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-09 09:29:44 -0500

    feat: :memo: Metodo repeat()
    descripcion del metodo que nos permite repetir elementos

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 
-".matchAll(regex) :Retorna un interador de todos los resultados que coinciden con una cadena de una expresión regular, incluyendo grupos capurados"
+
+    ".repeat(n): Repite la cadena N veces"
 
 
-   
-// EJEMPLOS
-const regexp = /t(e)(st(\d?))/g;
-const str = 'test1test2';
+// EJEMPLO 1
+let ejemplo = "hola"
+let print = ejemplo.repeat(2)
+console.log(print)
 
-const array = [...str.matchAll(regexp)];
-
-console.log(array[0]);
-// Expected output: Array ["test1", "e", "st1", "1"]
-
-console.log(array[1]);
-// Expected output: Array ["test2", "e", "st2", "2"]
