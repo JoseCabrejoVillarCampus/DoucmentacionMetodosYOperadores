ca968d7a17c12af9d0b53e071e938dbe047e3d9a
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-14 08:22:09 -0500

    feat: :sparkles: Funciones y Creacion de Funciones
    Introduccion a funciones y las formas de crearlas en JS

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-//                              Operadores lógicos
+//                                  FUNCIONES
 
 
-//Los operadores lógicos se utilizan normalmente con valores booleanos (lógicos); cuando lo son, 
-//devuelven un valor booleano. Sin embargo, los operadores && y || en realidad devuelven el valor 
-//de uno de los operandos especificados, por lo que si estos operadores se utilizan con valores no 
-//booleanos, pueden devolver un valor no booleano. Los operadores lógicos se describen en la siguiente tabla.
+//funciones también pueden ser tipos de datos:
+
+typeof function () {}; // 'function'
+
 //
-//                                 =>AND Lógico (&&) 	
+//                               =>CREACION DE FUNCIONES 	
 //
-//expr1 && expr2 	Devuelve expr1 si se puede convertir a false; de lo contrario, devuelve expr2. Por lo tanto, 
-//cuando se usa con valores booleanos, && devuelve true si ambos operandos son true; de lo contrario, devuelve false.
-//
-//                                 =>OR lógico (||) 	
-//
-//expr1 || expr2 	Devuelve expr1 si se puede convertir a true; de lo contrario, devuelve expr2. Por lo tanto, 
-//cuando se usa con valores booleanos, || devuelve true si alguno de los operandos es true; si ambos son falsos, devuelve false.
-//
-//                                =>NOT lógico (!) 	
-//
-//!expr 	Devuelve false si su único operando se puede convertir a true; de lo contrario, devuelve true.
+//Las funciones pueden ser creadas de 3 formas:
+
+//         1. Por Declaración.
+//         2. Por Expresión.	
+//         3. Mediante Constructor de Objeto.
+
+
+function campus(p1) { } // por declaracion 	
+
+var campus = function(p1) { } 	//por  expresion
+
+new Function(p1, code);  // por constructor de objeto
+
+
+
+
+
