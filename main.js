d07e21d54c949468a987bc864d9dbee59c330996
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-14 09:00:33 -0500

    feat: :sparkles: Funciones Anonimas
    Las funciones anonimas descritas y su posible uso

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-//                                  FUNCIONES COMO OBJETOS
+//                                  FUNCIONES ANONIMAS(LAMBDA)
 
 
-//En JavaScript tod puede ser un objeto. Sin embrago es algo que no suele usarce en produccion.
+//Son un tipo de funcion que se declaran sin nokmbre de funcion y se alojan al interior de una variable,
+//haciendo referencia a ella cada vez que queramos usarla.
+//Si ejecutamos la variable, ejecutamos la funcion d¿que contien la variable, de lo contrario nos devuelve 
+//la funcion en sí
+
+//La diferencia entre funciondes declaradas y funciones por expresion, es que por expresion solo estan disponibles
+//despues de la iniciacion de la variable, si la ejecutamos antes de declararlas nos dara un error. 
 
 
-
-const hora = new Function("return 'Es la una';");
-
-hora();
-
-
+const hora = function () {
+    return "Es la una";
+  };
+  
+  hora; // ƒ () { return 'Es la una'; }
+  hora(); // 'Es la una'
