26e2752cbef8d621192c522867964591c01b3a3c
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-14 08:29:28 -0500

    feat: :sparkles: Funciones por declaracion
    descripcion y ejemplo de funciones por declaracion

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
-//                                  FUNCIONES
+//                                  FUNCIONES POR DECLARACION
 
 
-//funciones también pueden ser tipos de datos:
-
-typeof function () {}; // 'function'
-
-//
-//                               =>CREACION DE FUNCIONES 	
-//
-//Las funciones pueden ser creadas de 3 formas:
-
-//         1. Por Declaración.
-//         2. Por Expresión.	
-//         3. Mediante Constructor de Objeto.
+//Nos permite declarar una función que existirá a lo largo de todo el codigo, es la mas popular
+// debido a la similitud con otros lenguajes de programacion.
+//JavaScript primero busca declaraciones de funciones y luego procesa el resto del codigo.
 
 
-function campus(p1) { } // por declaracion 	
-
-var campus = function(p1) { } 	//por  expresion
-
-new Function(p1, code);  // por constructor de objeto
+function Hora() {
+    return "Es la Una";
+  }
+  
+  Hora();
+  typeof Hora; 
 
 
 
